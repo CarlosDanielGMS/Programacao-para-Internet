24adc1567d0c23e55d3bd028537c2944493cdb87
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-02 20:46:47 -0300

    Questao 06 finalizada

---
+++ JavaScript/Atividade-01/Questao-06/scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById('imagem-lampada').addEventListener('click', function() {
+    document.getElementById('imagem-lampada').src="https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada-on.jpg?raw=true";
+});
