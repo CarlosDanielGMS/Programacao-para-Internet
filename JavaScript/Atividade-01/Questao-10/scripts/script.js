d0c4a0c0d448d13162773ee27468e201d428a81c
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-02 22:58:56 -0300

    Questao 10 finalizada

---
+++ JavaScript/Atividade-01/Questao-10/scripts/script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-soma').addEventListener('click', function () {
+    var soma1 = Number(document.getElementById('soma1').value);
+    var soma2 = Number(document.getElementById('soma2').value);
+    if (!isNaN(soma1) && !isNaN(soma2)) {
+        var resultadoSoma = soma1 + soma2;
+        document.getElementById('resultado-soma').innerHTML = "Resultado da soma:" + resultadoSoma;
+    }
+    else {
+        document.getElementById('resultado-soma').innerHTML = "Digite dois números válidos";
+    }
+});
