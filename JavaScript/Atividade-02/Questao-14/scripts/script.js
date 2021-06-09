16f5fb4b6511ae58d53a76787a02f8437dca32ac
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-09 19:03:33 -0300

    Questão 14 finalizada

---
+++ JavaScript/Atividade-02/Questao-14/scripts/script.js
@@ -0,0 +1,7 @@
+var divResultado = document.getElementById('resultado');
+
+function load () {
+    divResultado.innerHTML = "Esse texto foi inserido pelo JavaScript...";
+}
+
+window.onload = load;
