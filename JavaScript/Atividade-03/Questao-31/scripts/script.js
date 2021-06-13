dfcd5355b7c6967a3eefb052d73dd8b4d35c3e66
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-13 20:01:35 -0300

    Questão 31 finalizada

---
+++ JavaScript/Atividade-03/Questao-31/scripts/script.js
@@ -0,0 +1,12 @@
+var divResultado = document.getElementById('resultado');
+var numero = 0;
+
+for (var i = 1; i <= 10; i++) {
+    for (var n = 1; n <= 10; n++) {
+        divResultado.innerHTML += i + " x " + n + " = " + i * n + "<br>";
+
+        if (n == 10){
+            divResultado.innerHTML += "-----------------<br>";
+        }
+    }
+}
