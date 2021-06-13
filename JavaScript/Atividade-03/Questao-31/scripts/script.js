8175930f68b31446831a3f3ae70ae29fc4e19616
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-13 20:02:09 -0300

    Questão 31 formatada

--- JavaScript/Atividade-03/Questao-31/scripts/script.js
+++ JavaScript/Atividade-03/Questao-31/scripts/script.js
@@ -5,7 +5,7 @@ for (var i = 1; i <= 10; i++) {
     for (var n = 1; n <= 10; n++) {
         divResultado.innerHTML += i + " x " + n + " = " + i * n + "<br>";
 
-        if (n == 10){
+        if (n == 10) {
             divResultado.innerHTML += "-----------------<br>";
         }
     }
