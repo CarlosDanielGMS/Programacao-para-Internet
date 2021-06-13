0f4ff1f933f7a058f51bfc635e8958e8424a3e22
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-13 19:43:56 -0300

    Questão 30 finalizada

---
+++ JavaScript/Atividade-03/Questao-30/scripts/script.js
@@ -0,0 +1,9 @@
+var divResultado = document.getElementById('resultado');
+var numero = 0;
+
+for (var i = 0; i <= 10; i++) {
+    divResultado.innerHTML += numero + "<br>";
+
+    numero++;
+    numero++;
+}
