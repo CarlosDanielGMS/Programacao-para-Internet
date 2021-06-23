ca28e9687b2cda1586e0686f2f164a1b5e8aadd1
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-06-23 17:56:23 -0300

    Atividade 02 finalizada

--- AdonisJS/Atividade-02/start/routes.ts
+++ AdonisJS/Atividade-02/start/routes.ts
@@ -21,5 +21,21 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+  return view.render('home')
+}).as('home');
+
+Route.get('/about', async ({ view }) => {
+  return view.render('about')
+}).as('about');
+
+Route.get('/services', async ({ view }) => {
+  return view.render('services')
+}).as('services');
+
+Route.get('/portfolio', async ({ view }) => {
+  return view.render('portfolio')
+}).as('portfolio');
+
+Route.get('/team', async ({ view }) => {
+  return view.render('team')
+}).as('team');
