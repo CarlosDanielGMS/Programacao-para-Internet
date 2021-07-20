ac72507cff7e2be1309c2705cb332d9f8cfea140
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-07-20 19:08:13 -0300

    Atividade 03 finalizada

---
+++ AdonisJS/Atividade-03/app/Models/Produto.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Produto extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public nome: string
+
+  @column()
+  public descricao: string
+  
+  @column()
+  public categoria: string
+
+  @column()
+  public valorUnitario: number
+  
+  @column()
+  public quantidadeEstoque: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
