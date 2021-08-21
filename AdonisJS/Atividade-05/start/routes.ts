a52cb44ada2f10c61b5c5e068237492661380e92
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2021-08-21 17:07:49 -0300

    Rotas de usuários criadas

--- AdonisJS/Atividade-05/start/routes.ts
+++ AdonisJS/Atividade-05/start/routes.ts
@@ -1,11 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'ProdutosController.visualizacao').as('ProdutosViewer')
-
 Route.get('/cadastrar', 'ProdutosController.cadastro').as('ProdutoRegister')
 Route.post('/cadastrar', 'ProdutosController.salvamento').as('ProdutoSaver')
-
 Route.get('/alterar/:id', 'ProdutosController.alteracao').as('ProdutoChange')
 Route.post('/alterar/:id', 'ProdutosController.alterar').as('ProdutoAlter')
-
 Route.get('/remover/:id', 'ProdutosController.remocao').as('ProdutoRemove')
+
+Route.get('/usuarios', 'UsuariosController.listagem').as('listagemUsuarios')
+Route.get('/usuarios/cadastro', 'UsuariosController.cadastro').as('cadastroUsuario')
+Route.post('/usuarios/cadastrar', 'UsuariosController.cadastrar').as('cadastrarUsuario')
+Route.get('/usuarios/remover/:id', 'UsuariosController.remover').as('removerUsuario')
